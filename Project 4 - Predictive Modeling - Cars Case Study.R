917f2964ed007c42968587cc40f9f56f7fa53c7f
ahmasirier <60221204+ahmasirier@users.noreply.github.com>
2020-07-30 02:58:35 +0300

    commit

--- Project 4 - Predictive Modeling - Cars Case Study.R
+++ Project 4 - Predictive Modeling - Cars Case Study.R
@@ -7,7 +7,6 @@
 library(readr)
 library(ggplot2) 
 
-
 #setting up working directory
 setwd("C:/Users/ahmasiri/Desktop/PGP DSBA/Data/Project 4 - Cars Case Study")
 #reading data from csv file to Cars variable and view it
@@ -28,16 +27,6 @@ Cars[is.na(Cars)] <- 1
 #check if ther is any NA value in dataset
 anyNA(Cars)
 
-
-#outlier identification
-#Salary
-boxplot.stats(Cars$Salary)$out 
-#Age
-boxplot.stats(Cars$Age)$out 
-#Work.Exp
-boxplot.stats(Cars$Work.Exp)$out 
-#Distance
-boxplot.stats(Cars$Distance)$out
 
 # EDA 
 
@@ -243,6 +232,331 @@ ggplot(Cars,
        x = "license",
        title = "Employee license by Transport") +
   geom_bar(position = "stack") #specifying the type of bar chart as stacked
+
+
+
+
+
+
+# we are convering dependent varible to 1 and 0 where 1 indicate Cars and 0 indicates others
+Cars$BiTransport = ifelse(Transport == "Car",1,0)
+
+set.seed(300)
+library(caTools)
+spl = sample.split(Cars$BiTransport, SplitRatio=0.75)
+train = subset(Cars, spl ==T)
+test = subset(Cars, spl==F)
+
+
+
+#KNN
+
+library(class)
+set.seed(1)
+knnmod <- caret::train(Transport ~ .,
+                       method     = "knn",
+                       tuneGrid   = expand.grid(k = 2:51),
+                       metric     = "Accuracy",
+                       preProcess = c("scale"),
+                       data       = train)
+knnmod
+
+na.omit(train)
+
+model_knn=knn(train[,c(3,4,8)],test[,c(3,4,8)],train$Transport,k=19)
+
+caret::confusionMatrix(test$Transport,model_knn,positive="Car")
+
+lrmod <- caret::train(BiTransport ~ Engineer+MBA+license,
+                      method     = "glm",
+                      metric     = "Sensitivity",
+                      data       = train)
+
+lrpred<-predict(lrmod,newdata=test)
+lrpred
+
+Name = c("Naive_Bayes", "KNN", "Logistic_Regression")
+Accuracy = c(0.93,0.94,0.92)
+Sensitivity=c(0.98,1,0.87)
+Specificity=c(0.90,0.91,0.97)
+output = data.frame(Name,Accuracy,Sensitivity,Specificity)
+output
+
+
+
+
+
+#naiveBayes
+
+library(e1071) # to build a naive bayes model
+library(ROCR)
+
+model<-naiveBayes(BiTransport~.,data=train)
+model
+
+# generating the probabilities in prediction
+ypred<-predict(model, newdata = test, type="raw")
+plot(test$BiTransport,ypred[,2])
+
+# generating the class in prediction
+pred<-predict(model,newdata=test)
+
+
+
+p_test<-prediction(ypred[,2], test$BiTransport)
+perf<-performance(p_test,"tpr", "fpr")
+plot(perf,colorize = TRUE)
+
+cutoffs <-
+  data.frame(
+    cut = perf@alpha.values[[1]],
+    fpr = perf@x.values[[1]],
+    tpr = perf@y.values[[1]]
+  )
+
+head(cutoffs)
+
+cutoffs <- cutoffs[order(cutoffs$tpr, decreasing=TRUE),]
+head(subset(cutoffs, fpr < 0.1))
+
+class_prediction_with_new_cutoff = ifelse(ypred[, 2] >= 0.0129, 1, 0)
+new_confusion_matrix = table(test$BiTransport, class_prediction_with_new_cutoff)
+
+
+new_accuracy = sum(diag(new_confusion_matrix)) / sum(new_confusion_matrix)
+new_accuracy
+
+new_sensitivity = new_confusion_matrix[2,2] / sum(new_confusion_matrix[2, ])
+new_sensitivity
+
+new_specificity = new_confusion_matrix[1,1] / sum(new_confusion_matrix[1, ])
+new_specificity
+
+AUC_NB=performance(p_test,"auc")@y.values
+AUC_NB
+
+ks_nb = max(attr(perf,'y.values')[[1]] - attr(perf,'x.values')[[1]])
+ks_nb
+
+GINI_NB=2*AUC_NB[[1]]-1
+GINI_NB
+
+
+
+
+
+# Logistic Regression
+
+
+## Check split consistency
+prop.table(table(train$BiTransport))
+prop.table(table(test$BiTransport))
+prop.table(table(Cars$BiTransport))
+
+
+
+
+
+LRmodel = glm(BiTransport~ ., data = train, family= binomial)
+summary(LRmodel)
+
+library(MASS)
+log_model = stepAIC(LRmodel, direction = "both",k=5) #loosely speaking K=5, represents (P < 0.02)
+summary(log_model)
+
+
+
+library(caret)
+varImp(log_model)
+
+# convert to data frame
+l = data.frame(varImp(log_model))
+l <- cbind(newColName = rownames(l), l)
+rownames(l) <- 1:nrow(l)
+
+# soritng the imprtance of varaible 
+l[with(l, order(-Overall)), ]
+
+
+# 
+exp(0.0296)
+P = 0.49
+
+
+# prediction on test dataset
+predTrain = predict(log_model, newdata= train, type="response")
+tb = table(predTrain>0.50,train$BiTransport)
+tb
+print('accuracy is ')
+sum(diag(tb))/sum(tb)
+
+
+# prediction on test dataset
+predTest = predict(log_model, newdata= test, type="response")
+tb = table(predTest >0.50,test$BiTransport)
+tb
+print('accuracy is ')
+sum(diag(tb))/sum(tb)
+
+
+
+#par(mfrow=c(1,2))
+p0 <- prediction(predTrain,train$BiTransport)
+p1 <- performance(p0, "tpr", "fpr")
+plot(p1, main = "ROC Curve" ,colorize = TRUE)                        ## logistic regression model
+AUC  <- as.numeric(performance(p0, "auc")@y.values) ## AUC  = 0.9083176
+gini <- 2*AUC - 1                                   ## gini = 0.8166352
+KS   <- max(p1@y.values[[1]] - p1@x.values[[1]])    ## KS   = 0.6511416
+
+
+print('AUC')
+AUC
+
+print('KS')
+KS
+
+
+p0 = prediction(predTrain,train$BiTransport) 
+p1 = performance(p0,"tpr","fpr")
+plot(p1, main = "ROC Curve" ,colorize = TRUE)
+
+str(p1)
+
+# perf_rf_model2 is an S4 class. For S4 classes we use @ to access the slots (similar to how we use $)
+
+cutoffs <-
+  data.frame(
+    cut = p1@alpha.values[[1]],
+    fpr = p1@x.values[[1]],
+    tpr = p1@y.values[[1]]
+  )
+
+head(cutoffs)
+View(cutoffs)
+
+cutoffs <- cutoffs[order(cutoffs$tpr, decreasing=TRUE),]
+head(subset(cutoffs, fpr < 0.2))
+
+
+class_prediction_with_new_cutoff = ifelse(predTrain>= 0.24, 1, 0)
+new_confusion_matrix = table(train$BiTransport,class_prediction_with_new_cutoff )
+new_confusion_matrix
+
+new_accuracy = sum(diag(new_confusion_matrix)) / sum(new_confusion_matrix)
+new_accuracy
+
+new_sensitivity = new_confusion_matrix[2,2] / sum(new_confusion_matrix[2, ])
+new_sensitivity
+
+new_specificity = new_confusion_matrix[1,1] / sum(new_confusion_matrix[1, ])
+new_specificity
+
+class_prediction_with_new_cutoff = ifelse(predTest>= 0.24, 1, 0)
+new_confusion_matrix = table(test$BiTransport ,class_prediction_with_new_cutoff)
+new_confusion_matrix
+
+new_accuracy = sum(diag(new_confusion_matrix)) / sum(new_confusion_matrix)
+new_accuracy
+
+new_sensitivity = new_confusion_matrix[2,2] / sum(new_confusion_matrix[2, ])
+new_sensitivity
+
+new_specificity = new_confusion_matrix[1,1] / sum(new_confusion_matrix[1, ])
+new_specificity
+
+
+
+
+
+
+library(readr)
+library(ggplot2) 
+
+#setting up working directory
+setwd("C:/Users/ahmasiri/Desktop/PGP DSBA/Data/Project 4 - Cars Case Study")
+#reading data from csv file to Cars variable and view it
+Cars <- read.csv("Cars-dataset.csv")
+attach(Cars)
+
+
+#logistic regression
+german_logistic <- glm(Transport~., data=train, family=binomial(link="logit"))
+test$log.pred<-predict(german_logistic, test[1:8], type="response")
+#in the above line of code we have noted the results of the above two lines in the column called log.pred
+table(test$Transport,test$log.pred>0.5)
+#we are comapring the predicted values and given values. Anything above 0.5 will be a yes from the above code
+
+
+#The above resut says that for our not default we predicted 7106 correctly.
+#But when for the ones which did default we only predicted it correctly just for 12 times 
+#that it was going to default. The above confusion matrix might give us a very high 
+#probability of us being correct but we have to remember that we predicted most of 
+#the minority class wrongly.
+
+
+
+#knn
+#knn compare
+library(class)
+knn_fit<- knn(train = train[,c(3,4,8)], test = test[,c(3,4,8)], cl= train[,8],k = 3,prob=TRUE)
+#here were have predicted the  minority class all wrong. But we still 
+#do have a high probability of being correct.
+#Is it a good model though?
+table(test[,9],knn_fit)
+#KNN in this case is not a good algorithm to use in this case.
+
+
+
+#naive bayes
+library(e1071)
+nb_gd<-naiveBayes(x=train[,c(3,4,8)], y=as.factor(train[,9]))
+#do make sure that your dependent variable is a factor
+pred_nb<-predict(nb_gd,newdata = test[,c(3,4,8)])
+table(test[,9],pred_nb)
+#Here, we see that the Naive-Bayes algorithm definitely works better than KNN.
+
+#loading a few libraries
+library(gbm)          # basic implementation using AdaBoost
+library(xgboost)      # a faster implementation of a gbm
+library(caret)        # an aggregator package for performing many machine learning models
+
+
+## Bagging
+
+library(ipred)
+library(rpart)
+
+#we can modify the maxdepth and minsplit if needed
+#r doc, https://www.rdocumentation.org/packages/ipred/versions/0.4-0/topics/bagging
+Cars.bagging <- bagging(Transport ~.,
+                        data=train,
+                        control=rpart.control(maxdepth=5, minsplit=4))
+
+test$pred.Transport <- predict(Cars.bagging, test)
+
+table(test$Transport,test$pred.Transport)
+
+
+
+#Boosting
+
+library(gbm)
+gbm.fit <- gbm(
+  formula = Transport ~ .,
+  data = train,
+  n.trees = 10000, #these are the number of stumps
+  interaction.depth = 1,#number of splits it has to perform on a tree (starting from a single node)
+  shrinkage = 0.001,#shrinkage is used for reducing, or shrinking the impact of each additional fitted base-learner(tree)
+  cv.folds = 5,#cross validation folds
+  n.cores = NULL, # will use all cores by default
+  verbose = FALSE#after every tree/stump it is going to show the error and how it is changing
+)  
+
+
+test$pred.Transport <- predict(gbm.fit, test,type="response" )
+#we have to put type="response" just like in logistic regression else we will have log odds
+
+table(test$Transport,head(test$pred.Transport,105))
 
 
 
